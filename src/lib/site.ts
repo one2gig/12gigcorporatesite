420b7d4fc544ee4acf5bb06c39bea25659f33c44
one2gig <one2gig@gmail.com>
2026-08-16 12:22:23 +0800

    Beta landing page updates

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -3,7 +3,7 @@ export const MARKETPLACE_URL = 'https://12gig.com/';
 export const CONTACT_EMAIL = 'contact@12gig.com';
 export const WHATSAPP_URL = 'https://wa.me/60123977896';
 export const WHATSAPP_DISPLAY = '+60 12-397 7896';
-export const BETA_WHATSAPP_GROUP_URL = 'https://chat.whatsapp.com/Gzmjn09HfL5ItmI5riDa7x';
+export const BETA_WHATSAPP_GROUP_URL = 'https://chat.whatsapp.com/Kw7GzXw4XDV3AJUDCbIiWl';
 export const GIG_WORKERS_ACT_URL =
   'https://jtksm.mohr.gov.my/sites/default/files/2026-01/Akta%20872%20-%20AKTA%20PEKERJA%20GIG%202025.pdf';
 
